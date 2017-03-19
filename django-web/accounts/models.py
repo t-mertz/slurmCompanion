26830219420d3b5340a385b045050659c9c8ff85
Thomas Mertz <thomas_mertz@live.com>
2017-03-19 12:41:56 +0100

    New models
    * Added message model
    * Added messenger settings model
    * Added contact model

--- django-web/accounts/models.py
+++ django-web/accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Contact(models.Model):
+    """A contact relation between users.
+
+    A user's contact list is made up of the `contact` entries.
+    This a one-sided relationship. Adding a UserB as contact to
+    UserA does not add UserA as contact to UserB.
+    """
+    user = models.ForeignKey(User)
+    contact = models.ForeignKey(User)
--- django-web/messenger/models.py
+++ django-web/messenger/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Message(models.Model):
+    """Message sent by one user to on or many others."""
+    content = models.TextField()
+    time_sent = models.DateTimeField()
+    sender = models.ForeignKey(User)
+    recipient = models.ForeignKey(User) # make one-to-many?
+    title = models.CharField()
+    attachment = models.FileField()
+    flag = models.BigIntegerField() # important
+
+class MessageSettings(models.Model):
+    """Settings for the messenger view."""
+    user = models.ForeignKey(User, unique=True)
+
