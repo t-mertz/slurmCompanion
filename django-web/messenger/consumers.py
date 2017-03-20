effdb7625b2dde057dfc042f5dddb7c5b88590e1
Thomas Mertz <Thomas Mertz>
2017-03-20 09:41:21 +0100

    Started implementation of send_msg
    * removed title and attachment from standard Message for now
    * send_msg now stores the message to the database (untested)

---
+++ django-web/messenger/consumers.py
@@ -0,0 +1,50 @@
+from channels.auth import channel_session_user
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+
+import json
+import datetime
+from .models import Message
+
+@channel_session_user
+def send_msg(message):
+
+    # decode message
+    msg_data = json.loads(message.content['text'])
+
+    recipient_id = msg_data['uid']
+    try:
+        recipient = User.objects.get(user_id=recipient_id)
+    except ObjectDoesNotExist:
+        error_txt = "Invalid recipient ID."
+    sender = message.user
+    msg_content = msg_data['content'] #should this be processed somehow?
+    time_sent = datetime.datetime.now()
+
+    #
+    # first we want to store the message in the database, so it doesn't get lost
+    #
+
+    # construct new Message instance
+    new_message = Message(content=msg_content,
+                          time_sent=time_sent,
+                          sender=sender,
+                          recipient=recipient
+                         )
+    new_message.save()
+
+    #
+    # now we send the message to the recipient
+    #
+
+
+
+
+    #
+    # now we send a status report to the user
+    #
+    status_report = "success"
+
+    if error_txt:
+        status_report = error_txt
+    message.reply_channel.send(status_report)
--- django-web/messenger/models.py
+++ django-web/messenger/models.py
@@ -4,14 +4,14 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Message(models.Model):
-    """Message sent by one user to on or many others."""
+    """Message sent by one user to one or many others."""
     content = models.TextField()
     time_sent = models.DateTimeField()
     sender = models.ForeignKey(User)
     recipient = models.ForeignKey(User) # make one-to-many?
-    title = models.CharField()
-    attachment = models.FileField()
-    flag = models.BigIntegerField() # important
+    #title = models.CharField()
+    #attachment = models.FileField()
+    #flag = models.BigIntegerField() # important
 
 class MessageSettings(models.Model):
     """Settings for the messenger view."""
