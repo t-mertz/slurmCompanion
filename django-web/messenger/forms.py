00b5b9bab970566c7a87b3fdb0b84c64c4e86ca3
Thomas Mertz <Thomas Mertz>
2017-04-10 17:54:56 +0200

    Fixed typo

--- django-web/messenger/forms.py
+++ django-web/messenger/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class NewMessageForm(forms.Form):
-    msg_text = forms.CharField(widget=forms.widgets.Textaxrea())
+    msg_text = forms.CharField(widget=forms.widgets.Textarea())
     recipient = forms.ChoiceField(choices=()) # choices should be (Contact.user==current_user).contact
